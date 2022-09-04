5a2be181c6a1d4f83e23ddeb391d0d292f4da2d9
kumackey <kumak1t09e0@gmail.com>
2022-09-04 21:38:58 +0900

    change terrafrom command management

--- setup.sh
+++
@@ -1,8 +0,0 @@
-brew install git-secrets
-git secrets --register-aws --global
-git secrets --install "$HOME/.git-templates/git-secrets"
-git config --global init.templatedir "$HOME/.git-templates/git-secrets"
-
-docker pull hashicorp/terraform:0.12.5
-
-echo 'DONE👍'
