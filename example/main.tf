e2fe5821aeeda995a9379d5bf3466989c5342e07
kumackey <kumak1t09e0@gmail.com>
2022-09-04 21:47:15 +0900

    try resource replaced case

--- example/main.tf
+++ example/main.tf
@@ -1,4 +1,13 @@
 resource "aws_instance" "example" {
   ami = "ami-0c3fd0f5d33134a76"
   instance_type = "t3.micro"
+
+  user_data = <<EOF
+#!/bin/bash
+yum install -y httpd
+systemctl start
+EOF
+  tags = {
+    Name = "example"
+  }
 }
