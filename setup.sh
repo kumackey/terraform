bb0ba80f06930dd60f1fa2750849f609493d268c
kumackey <kumak1t09e0@gmail.com>
2022-09-04 21:08:14 +0900

    add git-secrets

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
+brew install git-secrets
+git secrets --register-aws --global
+git secrets --install "$HOME/.git-templates/git-secrets"
+git config --global init.templatedir "$HOME/.git-templates/git-secrets"
+
 docker pull hashicorp/terraform:0.12.5
 
-./tf.sh --version
+echo 'DONE👍'
