6267950b9e38a5bd1f1f52b9c7fe3925e9152c7c
kumackey <kumak1t09e0@gmail.com>
2022-09-06 21:17:06 +0900

    variable

--- example/main.tf
+++ example/main.tf
@@ -1,13 +1,8 @@
+variable "example_instance_type" {
+  default = "t3.micro"
+}
+
 resource "aws_instance" "example" {
   ami = "ami-0c3fd0f5d33134a76"
-  instance_type = "t3.micro"
-
-  user_data = <<EOF
-#!/bin/bash
-yum install -y httpd
-systemctl start
-EOF
-  tags = {
-    Name = "example"
-  }
+  instance_type = var.example_instance_type
 }
