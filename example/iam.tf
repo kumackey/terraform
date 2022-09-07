dfec8cfd29b84475bd0bfd1445150384aa05a5e7
kumackey <kumak1t09e0@gmail.com>
2022-09-07 21:42:17 +0900

    implements iam module

---
+++ example/iam.tf
@@ -0,0 +1,48 @@
+variable "name" {}
+variable "policy" {}
+variable "identifier" {}
+
+resource "aws_iam_role" "default" {
+  name = var.name
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+}
+
+data "aws_iam_policy_document" "ec2_assume_role" {
+  statement {
+    actions = [
+      "sts:AssumeRole"]
+    principals {
+      identifiers = [
+        "ec2.amazonaws.com"]
+      type = "Service"
+    }
+  }
+}
+
+resource "aws_iam_policy" "default" {
+  name = var.name
+  policy = data.aws_iam_policy_document.allow_describe_regions.json
+}
+
+data "aws_iam_policy_document" "allow_describe_regions" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ec2:DescribeRegions"]
+    resources = [
+      "*"]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "default" {
+  role = aws_iam_role.default.name
+  policy_arn = aws_iam_policy.default.arn
+}
+
+output "iam_role_arn" {
+  value = aws_iam_role.default.arn
+}
+
+output "iam_role_name" {
+  value = aws_iam_role.default.name
+}
