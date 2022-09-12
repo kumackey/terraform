ddaab60b7969f2f978ad4a16f77e8dddfe805800
kumackey <kumak1t09e0@gmail.com>
2022-09-12 22:36:03 +0900

    security group module

---
+++ example/security_group/main.tf
@@ -0,0 +1,32 @@
+variable "name" {}
+variable "vpc_id" {}
+variable "port" {}
+variable "cider_blocks" {
+  type = list(string)
+}
+
+resource "aws_security_group" "default" {
+  name = var.name
+  vpc_id = var.vpc_id
+}
+
+resource "aws_security_group_rule" "ingress_example" {
+  from_port = var.port
+  protocol = "tcp"
+  security_group_id = aws_security_group.default.id
+  to_port = var.port
+  type = "ingress"
+}
+
+resource "aws_security_group_rule" "ingress_example" {
+  from_port = 0
+  protocol = "-1"
+  security_group_id = aws_security_group.default.id
+  to_port = 0
+  type = "egress"
+  cidr_blocks = var.cider_blocks
+}
+
+output "security_group_id" {
+  value = aws_security_group.default.id
+}
