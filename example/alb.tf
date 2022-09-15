3b71f139a8886594f1d784c567ac2306829ae770
kumackey <kumak1t09e0@gmail.com>
2022-09-16 07:57:35 +0900

    implements alb

--- example/alb.tf
+++ example/alb.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "example" {
   load_balancer_type = "application"
   internal = false
   idle_timeout = 60
-  enable_deletion_protection = true
+  enable_deletion_protection = false
 
   subnets = [
     aws_subnet.public_0.id,
@@ -69,26 +69,120 @@ resource "aws_lb_listener" "http" {
   }
 }
 
-data "aws_route53_zone" "example" {
-  name = "example.com"
-}
+//resource "aws_route53_zone" "example" {
+//  name = "test.example.com"
+//}
+//
+//resource "aws_route53_record" "example" {
+//  name = aws_route53_zone.example.name
+//  type = "A"
+//  zone_id = aws_route53_zone.example.zone_id
+//
+//  alias {
+//    name = aws_lb.example.dns_name
+//    evaluate_target_health = true
+//    zone_id = aws_lb.example.zone_id
+//  }
+//}
+//
+//output "domain_name" {
+//  value = aws_route53_record.example.name
+//}
 
-resource "aws_route53_zone" "test_example" {
-  name = "test.example.com"
-}
+//resource "aws_acm_certificate" "example" {
+//  domain_name = aws_route53_record.example.name
+//  subject_alternative_names = []
+//  validation_method = "DNS"
+//
+//  lifecycle {
+//    create_before_destroy = true
+//  }
+//}
+//
+//resource "aws_route53_record" "example_certificate" {
+//  name = aws_acm_certificate.example.domain_validation_options[0].resource_record_name
+//  type = aws_acm_certificate.example.domain_validation_options[0].resource_record_type
+//  records = [
+//    aws_acm_certificate.example.domain_validation_options[0].resource_record_value
+//  ]
+//  zone_id = aws_route53_zone.example.id
+//  ttl = 60
+//}
+//
+//resource "aws_acm_certificate_validation" "example" {
+//  certificate_arn = aws_acm_certificate.example.arn
+//  validation_record_fqdns = [
+//    aws_route53_record.example_certificate.fqdn
+//  ]
+//}
+//
+//resource "aws_lb_listener" "https" {
+//  load_balancer_arn = aws_lb.example.arn
+//  port = "443"
+//  protocol = "HTTPS"
+//  certificate_arn = aws_acm_certificate.example.arn
+//  ssl_policy = "ELBSecurityPolicy-2016-08"
+//  default_action {
+//    type = "fixed-response"
+//
+//    fixed_response {
+//      content_type = "text/plain"
+//      message_body = "これは『HTTPS』です"
+//      status_code = "200"
+//    }
+//  }
+//}
+//
+//resource "aws_lb_listener" "redirect_http_to_https" {
+//  load_balancer_arn = aws_lb.example.arn
+//  port = "8080"
+//  protocol = "HTTP"
+//  default_action {
+//    type = "redirect"
+//
+//    redirect {
+//      port = "443"
+//      protocol = "HTTPS"
+//      status_code = "HTTP_301"
+//    }
+//  }
+//}
 
-resource "aws_route53_record" "example" {
-  name = data.aws_route53_zone.example.name
-  type = "A"
-  zone_id = data.aws_route53_zone.example.zone_id
-
-  alias {
-    name = aws_lb.example.dns_name
-    evaluate_target_health = true
-    zone_id = aws_lb.example.zone_id
-  }
-}
-
-output "domain_name" {
-  value = aws_route53_record.example.name
-}
+//resource "aws_lb_target_group" "example" {
+//  name = "example"
+//  target_type = "ip"
+//  vpc_id = aws_vpc.example.id
+//  port = 80
+//  protocol = "HTTP"
+//  deregistration_delay = 300
+//
+//  health_check {
+//    path = "/"
+//    healthy_threshold = 5
+//    unhealthy_threshold = 2
+//    timeout = 5
+//    interval = 30
+//    matcher = 200
+//    port = "traffic-port"
+//    protocol = "HTTP"
+//  }
+//
+//  depends_on = [
+//    aws_lb.example]
+//}
+//
+//resource "aws_lb_listener_rule" "example" {
+//  listener_arn = aws_lb_listener.https.arn
+//  priority = 100
+//
+//  condition {
+//    field = "path-pattern"
+//    values = [
+//      "/*"
+//    ]
+//  }
+//  action {
+//    type = "forward"
+//    target_group_arn = aws_lb_target_group.example.arn
+//  }
+//}
